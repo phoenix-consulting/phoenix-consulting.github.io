da0524e0ced46fece8327a6ed6227e46e542d54b
Mohak Jain <mohakjain@gmail.com>
2021-02-26 02:01:27 -0800

    finished first draft

--- src/components/faq.jsx
+++ src/components/faq.jsx
@@ -7,33 +7,22 @@ export class faq extends Component {
         <div className="container">
           <div className="row">
            
-              <div className="about-text">
+              <div className="faq-text">
                 <h2>Frequently Asked Questions
                 </h2>
-                <p>{this.props.data ? this.props.data.paragraph : 'loading...'}</p>
-                <h3>Register Now! </h3>
-                <div className="">
-
-                  <div className="col-sm-6">
-                  <a href={""} target="_blank" rel="noopener noreferrer"> 
-                  I'm a startup looking to attend! 
-                  
-                  </a>
-                  </div>
-
-                  <div className="col-sm-6">
-                  <a href={""} target="_blank" rel="noopener noreferrer"> 
-                  I'm an investor looking to attend! 
-                  </a>
-                  </div>
-
+                
+                {this.props.data ? this.props.data.map((d,i) => (
+                <div>
+                <h3> {d.question} </h3>
+                <p> {d.answer} </p>
+                </div>
+                )) : "Loading..."}
+                
                 </div>
               </div>
             </div>
           </div>
-        
-      </div>
-    )
+            )
   }
 }
 
