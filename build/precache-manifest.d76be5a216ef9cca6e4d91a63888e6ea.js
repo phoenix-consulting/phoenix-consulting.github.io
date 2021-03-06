a9890382cae1ca0be794a13dab5d505adfb6461f
Mohak Jain <mohakjain@gmail.com>
2021-03-06 12:51:30 -0800

    ??/

---
+++ build/precache-manifest.d76be5a216ef9cca6e4d91a63888e6ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f27ce68c7aaba86c6d72a96bd097a8",
+    "url": "/demoday-public/index.html"
+  },
+  {
+    "revision": "b0750f5c4ece330dd8cb",
+    "url": "/demoday-public/static/css/main.62d40da3.chunk.css"
+  },
+  {
+    "revision": "334eaf7bf699b8ea28c0",
+    "url": "/demoday-public/static/js/2.c126d411.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demoday-public/static/js/2.c126d411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0750f5c4ece330dd8cb",
+    "url": "/demoday-public/static/js/main.2eac7fc7.chunk.js"
+  },
+  {
+    "revision": "94e97cc892f0778db84e",
+    "url": "/demoday-public/static/js/runtime-main.a3cfd621.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/readysethealth.io/precache-manifest.709bc15b5a3d60f755ac979d852c2705.js"
+  "/demoday-public/precache-manifest.d76be5a216ef9cca6e4d91a63888e6ea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/readysethealth.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/demoday-public/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
