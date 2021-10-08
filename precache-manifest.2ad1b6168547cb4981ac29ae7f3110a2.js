b49da67783a1efd968f7b93f061f31a8e8da3afc
Mohak Jain <mohakjain@Conjecture.local>
2021-10-08 13:45:45 -0700

    Updates

--- precache-manifest.2ad1b6168547cb4981ac29ae7f3110a2.js
+++ precache-manifest.2ad1b6168547cb4981ac29ae7f3110a2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17a8df93de7146a99fa9a87d5a27d85b",
+    "revision": "ca639ba51cee0fa949fc1d644098c0b4",
     "url": "/index.html"
   },
   {
-    "revision": "204eefc9ee89d811924a",
+    "revision": "b5bffcb3ebcb74ceb06d",
     "url": "/static/css/main.eaf50d03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c126d411.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "204eefc9ee89d811924a",
-    "url": "/static/js/main.ccfd6860.chunk.js"
+    "revision": "b5bffcb3ebcb74ceb06d",
+    "url": "/static/js/main.d52c1a74.chunk.js"
   },
   {
     "revision": "8b9ad81330d746c33b1d",
